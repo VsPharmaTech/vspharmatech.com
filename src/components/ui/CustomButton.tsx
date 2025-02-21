d77bda1079ce6d231244b8fa458d1000278883f3
jay-shinde <07jayesh.shinde@gmail.com>
2025-02-22 00:01:50 +0530

    removed the commented code

--- src/components/ui/CustomButton.tsx
+++ src/components/ui/CustomButton.tsx
@@ -27,27 +27,3 @@ export default function Button({ variant = 'primary', children, href }: ButtonPr
   );
 }
 
-// import React from 'react';
-
-// interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-//   variant?: 'primary' | 'secondary';
-//   children: React.ReactNode;
-// }
-
-// export default function Button({ variant = 'primary', children, ...props }: ButtonProps) {
-//   return (
-//     <button
-//       {...props}
-//       className={`
-//         px-6 py-2 rounded-md font-medium font-roboto transition-colors flex items-center gap-2
-//         ${variant === 'primary'
-//           ? 'bg-blue-600 text-white hover:bg-blue-800'
-//           : 'bg-white text-blue-600 border border-blue-600 hover:bg-blue-50'}
-//       `}
-//     >
-//       {children}
-//       <img src="/ArrowF.svg" alt="Arrow" className="h-full gap-x-2" />
-//     </button>
-//   )
-// }
-
